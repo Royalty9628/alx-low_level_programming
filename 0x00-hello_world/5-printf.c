356ca99dc9e6b773e40a5d8d2a414e8426a2c0e2
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-06-27 16:01:19 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
- * main - print the phrase "with proper grammar, 
+ * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
