b3fd8bc73c6cdb4019e200c40d8e9d687a0f3833
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-07-01 11:15:22 -0700

    100-print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@ int main(void)
 	int n;
 	int i = 0;
 
-	for (n = 0 ; n < 100 ; n++)
+	for (n = 0 ; n < 90 ; n++)
 	{
 		putchar(n / 10 + '0');
 		putchar(i + '0');
-		if (n < 99)
+		if (n < 90)
 		{
 			putchar(',');
 			putchar(32);
