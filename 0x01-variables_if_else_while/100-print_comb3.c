e39d9214dbe7d16bb5bbd79500e0f09c3d6705ed
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-07-01 11:10:20 -0700

    100-print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        a comma followed by a space, in ascending order.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	int i = 0;
+
+	for (n = 0 ; n < 100 ; n++)
+	{
+		putchar(n / 10 + '0');
+		putchar(i + '0');
+		if (n < 99)
+		{
+			putchar(',');
+			putchar(32);
+		}
+		i++;
+		if (i > 9)
+		{
+			i = 0;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
