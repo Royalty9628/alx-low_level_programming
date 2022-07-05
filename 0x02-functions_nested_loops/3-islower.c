7115c4e0958641dcce65e09cb8a87e59ccbfaca6
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-07-05 14:45:42 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - _islower - tests whether a character is a
+ * _islower - tests whether a character is a
  * lowercase letter from the English alphabet.
  * @c: character to test.
  * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
