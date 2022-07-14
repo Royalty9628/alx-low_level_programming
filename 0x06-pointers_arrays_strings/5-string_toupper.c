85acacda260efeea02d5e6c5a4b211f6ac634ece
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-07-14 05:46:32 -0700

    capitalize string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - functon that changes all lowercase to uppercase
+ * @str: pointer
+ * Return: Always 0
+ */
+
+char *string_toupper(char *str)
+{
+	int X = 0;
+
+	for (str[X] != '\0'; X++)
+	{
+
+		if (str[X] <= 'z' && str[X] >= 'a')
+		{
+			str[X] = str[X] - 32;
+		}
+	}
+
+	return (str);
+}
