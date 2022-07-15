eb63ddd55f6ee61f16b93fe9a54671c350fe50e7
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-07-15 00:43:52 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,8 +8,8 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-  /**
-   * write your line of code here ...
+  /*
+   * write your line of code here...
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
@@ -17,7 +17,7 @@ p = &n;
    * - you are not allowed to code anything else than this line of code
    */
 *(p + 5) = 98;
-   /* ...so that this prints 98\n */
+  /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
