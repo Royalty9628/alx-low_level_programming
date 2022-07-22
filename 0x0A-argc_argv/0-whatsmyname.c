96cafb17fead661d190f207132fb9782588170f0
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-07-22 16:37:13 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name.
+ * @argc: int
+ * @argv: list
+ * Return: Always 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
