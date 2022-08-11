6c056437e03109af575ed049eff117eece8d08a2
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-08-11 13:07:55 -0700

    prints all the element

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: pointer to the start of the list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *tmp;
+	unsigned int A;
+
+	tmp = h;
+
+	for (A = 0; tmp; A++)
+	{
+		printf("[%u] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
+	}
+	return (A);
+}
