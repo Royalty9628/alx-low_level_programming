0358ef028a803b03b3d234ae20d18ac7ca50afce
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-08-11 13:19:04 -0700

    print all element

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
-	const list_t *tmp;
-	unsigned int A;
+	unsigned int i;
 
-	tmp = h;
-
-	for (A = 0; tmp; A++)
+	i = 0;
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
 	{
-		printf("[%u] %s\n", tmp->len, tmp->str);
-		tmp = tmp->next;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
 	}
-	return (A);
+	return (i);
 }
