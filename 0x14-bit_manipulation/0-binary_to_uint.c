b17426825034d353266076fee4e77b3117ef3a63
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-08-18 05:53:31 -0700

    error fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int j;
+	int i;
 	unsigned int num;
 
 	num = 0;
-
 	if (!b)
 		return (0);
-
-	for (j = 0; b[j] != '\0'; j++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (*(b + j) == '1')
-			num = (num << 1) | 1;
-		else if (*(b + j) == '0')
-			num <<= 1;
-		else
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
 	return (num);
 }
