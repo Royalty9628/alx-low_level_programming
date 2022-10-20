1ef74a1b12907ac09f2730b6efe0c5ee5e0241bc
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-10-20 14:03:10 -0700

    Check Error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,51 +1,73 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: the hashs table I want to add or update the key/value to
- * @key: is the str passed to djb2 (tha key )
- * @value: value associated with the key
- * Return: 1 if succeeded or 0 if fail
+ * make_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: for the node
+ *
+ * Return: the new node, or NULL on failure
  */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
 
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
+
+/**
+ * hash_table_set - sets a key to a value in the hash table
+ * @ht: hash table to add elemt to
+ * @key: key for the data
+ * @value: data to store
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *hash_nodes;
-	hash_node_t *new_node;
 	unsigned long int index;
+	hash_node_t *hash_node, *tmp;
+	char *new_value;
 
-	if (!ht || !key)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
-	/* first I need to know where must be indexed the new node in the ht */
 	index = key_index((const unsigned char *)key, ht->size);
-	/* hashnodes points to the array of the ht with the respective idx */
-	hash_nodes = ht->array[index];
-	/* in case of key/value update */
-	while (hash_nodes)
+	tmp = ht->array[index];
+	while (tmp != NULL)
 	{
-		if (strcmp(hash_nodes->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			/* delete the actual value in the hash table */
-			free(hash_nodes->value);
-			/* the passed value must be duplicated, task cond. */
-			hash_nodes->value = strdup(value);
-			if (hash_nodes->value == NULL)
+			new_value = strdup(value);
+			if (new_value == NULL)
 				return (0);
+			free(tmp->value);
+			tmp->value = new_value;
 			return (1);
 		}
-		hash_nodes = hash_nodes->next;
+		tmp = tmp->next;
 	}
-	/* in case of adding a new node to the hash table */
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	hash_node = make_hash_node(key, value);
+	if (hash_node == NULL)
 		return (0);
-	/* copy of key because is a constant and can assing it  */
-	new_node->key = strdup(key);
-	/* the passed value must be duplicated, task cond. */
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-		return (0);
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
 	return (1);
 }
